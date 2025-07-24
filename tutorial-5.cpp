6d7c66b0717df95bd17b0506692643507a071d04
Ganesh-Tata <ganeshtata115@gmail.com>
2025-07-24 11:26:04 +0530

    Update tutorial-5.cpp

--- tutorial-5.cpp
+++ tutorial-5.cpp
@@ -11,3 +11,19 @@ int main()
     std::cout <<firstName<<'\n';
     return 0;
 }
+
+#include<iostream>
+#include<vector>
+//typedef std::vector<std::pair<std::string , int>>pairlist_t;
+//typedef std::string text_t;
+//typedef int number_t;
+using text_t=std::string;
+using number_t=int;
+int main()
+{
+    text_t firstName="Bro";
+    number_t age=20;
+    std::cout<<age<<'\n';
+    std::cout <<firstName<<'\n';
+    return 0;
+}
