706069ecad1986fffb505247462d525c1ee90b28
Ganesh-Tata <ganeshtata115@gmail.com>
2025-07-24 10:59:47 +0530

    Create tutorial -1.cpp

---
+++ tutorial -1.cpp
@@ -0,0 +1,25 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+    // Write C++ code here
+    int age=20;
+    int salary=2.5;
+    
+    double gpa=3.55;
+    double temp=22.2;
+    
+    char grade='A';
+    char initial='c';
+    
+    bool student=false;
+    bool power=true;
+    
+    std::string name="BRO" ;
+    std::string day="Monday";
+    
+    std::cout << name;
+    std::cout << day;
+
+    return 0;
+}
